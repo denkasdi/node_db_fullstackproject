23ed6414d5e69339062f0abc3d6a3922c0a13cb1
Windows10 <denrokokasdi@gmail.com>
2023-07-14 12:38:39 +0800

    add ssl

--- db/index.js
+++ db/index.js
@@ -5,7 +5,7 @@ import "dotenv/config";
 const db = new Pool({
 	host: process.env.DB_HOST || "localhost",
 	user: process.env.DB_USER || "postgres",
-	database: process.env.DB_DATABSE || "my-blog",
+	database: process.env.DB_DATABASE || "my-blog",
 	password: process.env.DB_PASSWORD || "Messi@950131",
 	ssl: process.env.DB_SSL || true,
 	max: 20,
