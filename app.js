ac6780ec91d40f84129a99f85be43f16470caa6b
Windows10 <denrokokasdi@gmail.com>
2023-07-15 09:29:55 +0800

    add ssl

--- app.js
+++ app.js
@@ -121,6 +121,26 @@ app.get("/api/users/:username", async (req, res) => {
 	res.status(200).json({ serverRes });
 });
 
+app.delete("/api/usersdelete/:username", async (req, res) => {
+	const username = req.params.username;
+
+	const dbRes = await query("DELETE FROM users WHERE username = $1", [
+		username,
+	]);
+
+	if (dbRes.rowCount === 0) {
+		const notFoundRes = {
+			message: "No user found with the provided username!",
+		};
+		return res.status(404).json(notFoundRes);
+	}
+
+	const successRes = {
+		message: "User has been deleted successfully!",
+	};
+	res.status(200).json(successRes);
+});
+
 app.get("/api/location/:lokasi", async (req, res) => {
 	const lokasi = req.params.lokasi;
 	const dbRes = await query("SELECT * FROM users WHERE lokasi=$1", [lokasi]);
