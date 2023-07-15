083e3d91c8c9cbe3dbc6fe87170e91b3fb196aa3
Windows10 <denrokokasdi@gmail.com>
2023-07-15 15:08:37 +0800

    add ssl

--- controller/user/test/listAll.test.js
+++ controller/user/test/listAll.test.js
@@ -1,13 +1,10 @@
 import request from "supertest";
 import app from "../../../app.js";
 
-describe("Try get location of user without entering location", () => {
-	test("It should response with status code 404 and return message = Not found", async () => {
-		return request(app)
-			.get("/api/location")
-			.then((response) => {
-				expect(response.statusCode).toBe(404);
-				//expect(response.body.message).toEqual("Invalid request!");
-			});
+describe("Try get all users", () => {
+	test("It should response with status code 200 and return message = Ok", async () => {
+		const response = await request(app).get("/api/users");
+		expect(response.statusCode).toBe(200);
+		expect(response.body.status).toEqual("Ok");
 	});
 });
